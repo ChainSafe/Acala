9230929082a7813b8cff4fdba917d3dbc97f4af1
Matthias Seitz <matthias.seitz@outlook.de>
2021-09-15 14:42:01 +0200

    feat: add pint weights

--- runtime/mandala/src/lib.rs
+++ runtime/mandala/src/lib.rs
@@ -1980,7 +1980,7 @@ impl pint_saft_registry::Config for Runtime {
 	type Balance = Balance;
 	type AssetId = CurrencyId;
 	type Event = Event;
-	type WeightInfo = ();
+	type WeightInfo = pint_runtime_common::weights::pallet_saft_registry::WeightInfo<Runtime>;
 }
 
 impl pint_local_treasury::Config for Runtime {
@@ -1988,7 +1988,7 @@ impl pint_local_treasury::Config for Runtime {
 	type PalletId = TreasuryPalletId;
 	type Currency = Balances;
 	type Event = Event;
-	type WeightInfo = ();
+	type WeightInfo = pint_runtime_common::weights::pallet_local_treasury::WeightInfo<Runtime>;
 }
 
 impl pint_committee::Config for Runtime {
@@ -2002,7 +2002,7 @@ impl pint_committee::Config for Runtime {
 	type ProposalExecutionOrigin = pint_committee::EnsureMember<Self>;
 	type ApprovedByCommitteeOrigin = PintGovernanceOrigin<AccountId, Runtime>;
 	type Event = Event;
-	type WeightInfo = ();
+	type WeightInfo = pint_runtime_common::weights::pallet_committee::WeightInfo<Runtime>;
 }
 
 impl pint_asset_index::Config for Runtime {
@@ -2028,7 +2028,7 @@ impl pint_asset_index::Config for Runtime {
 	type TreasuryPalletId = TreasuryPalletId;
 	type Event = Event;
 	type StringLimit = AssetIndexStringLimit;
-	type WeightInfo = ();
+	type WeightInfo = pint_runtime_common::weights::pallet_asset_index::WeightInfo<Runtime>;
 }
 
 // Wrapper for the `AggregatedDataProvider` until chainlink pallet is supported
